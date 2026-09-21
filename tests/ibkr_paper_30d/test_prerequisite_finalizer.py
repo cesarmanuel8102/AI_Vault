e03dbdb3d738577a775ff10394ed180dc72627b7
cesarmanuel8102 <cesarmanuel8102@gmail.com>
2026-09-21 03:15:59 -0400

    Test fail-closed finalizer argument quoting

--- tests/ibkr_paper_30d/test_prerequisite_finalizer.py
+++ tests/ibkr_paper_30d/test_prerequisite_finalizer.py
@@ -28,6 +28,7 @@ def test_prerequisite_scripts_exist_and_never_arm_trading():
     assert "market_data_task_registered" in finalizer
     assert "Enable-LocalUser -Name $AuditorUser" in finalizer
     assert "Disable-LocalUser -Name $AuditorUser" in finalizer
+    assert "UNSAFE_ARGUMENT_VALUE" in finalizer
 
     market = MARKET_RUNNER.read_text(encoding="utf-8")
     assert '"330"' in market
